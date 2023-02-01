85a7c47dc45c880fef418671cadccf00c43ae6aa
nceeoh7 <nico@fabrique7.com>
2023-01-31 22:05:47 -0500

    Few comments

--- array-exercises.c
+++ array-exercises.c
@@ -10,6 +10,7 @@ int main()
 
     printf("The first letter of your name as an int is %i\n", name[0]);
 
+    //Example of explicit casting
     int letterInt = (int)name[0]; 
     printf("The first letter of your name as a cast int is %i\n", letterInt);
 
@@ -18,10 +19,12 @@ int main()
 
     char str[]= "hello world";
 
+    //How to figure out the size of data type array.
     int strLen = sizeof(str)/sizeof(str[0]);
 
     printf("The size of the \"hello world\" is %i elements.\n", strLen);
 
+    //Iterating over every element of char array and printing the null char
     for (int i = 0; i< strLen;i++){
         if(str[i]== '\0'){
             printf("\\0\n");
